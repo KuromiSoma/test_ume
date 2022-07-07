c88badc12819f52ccf5b10406ed2109d86ab3625
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2022-07-07 15:05:56 +0900

    first commit

---
+++ src/main/java/com/example/demo/UmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmeApplication.class, args);
+	}
+
+}
